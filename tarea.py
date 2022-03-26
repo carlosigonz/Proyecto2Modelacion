620caa870ab1f37cd4e215a626aef6004ce7c254
Carlos Gonzalez <carlosigonz@gmail.com>
2022-03-26 00:49:49 -0400

    Agregadas variables de Tarea

--- tarea.py
+++ tarea.py
@@ -1,6 +1,11 @@
 class Tarea:
-    def __init__(self,numero,descripcion,duracion,predecesor):
+    def __init__(self, numero, descripcion, duracion, predecesor):
         self.nombre = numero
         self.descripcion = descripcion
         self.duracion = duracion
         self.predecesor = predecesor
+        self.early_start = None
+        self.early_finish = None
+        self.late_start = None
+        self.late_finish = None
+        self.holgura = None
